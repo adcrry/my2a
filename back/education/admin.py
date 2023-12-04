d9351f0a6eac3f0d68d176ee65019d1b46081c5d
sinodine <sixtine.nodet@gmail.com>
2023-12-04 16:22:04 +0100

    improve admin panel

--- back/education/admin.py
+++ back/education/admin.py
@@ -2,7 +2,26 @@ from django.contrib import admin
 
 from .models import Student, Course, Department, Parcours
 
-admin.site.register(Student)
-admin.site.register(Course)
+
+class StudentAdmin(admin.ModelAdmin):
+    search_fields = ["name", "surname", "department__name", "parcours__name"]
+    list_filter = ["department", "parcours"]
+    list_display = ["name", "surname", "department", "parcours"]
+
+
+class CourseAdmin(admin.ModelAdmin):
+    search_fields = ["name", "code", "department__name"]
+    list_filter = ["department", "semester", "day"]
+    list_display = ["name", "code", "department"]
+
+
+class ParcoursAdmin(admin.ModelAdmin):
+    search_fields = ["name", "department__name"]
+    list_filter = ["department"]
+    list_display = ["name", "department"]
+
+
+admin.site.register(Student, StudentAdmin)
+admin.site.register(Course, CourseAdmin)
 admin.site.register(Department)
-admin.site.register(Parcours)
+admin.site.register(Parcours, ParcoursAdmin)
