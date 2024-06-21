8c9dc7b105f3e9e7e05c9021c111b7d539a363b1
adcrry <ducorroyalexandre@gmail.com>
2024-06-21 11:01:11 +0200

    fix: render => redirectgit add backgit add backgit add backgit add backgit add backgit add back!

--- back/education/views.py
+++ back/education/views.py
@@ -43,7 +43,7 @@ def auth_view(request):
         if request.user.is_authenticated and request.user.is_superuser:
             return redirect("/inspector/")
         elif request.user.is_authenticated:
-            return render("/")
+            return redirect("/")
         return render(request, "registration/login.html")
     username = request.POST.get("mail", "")
     password = request.POST.get("password", "")
